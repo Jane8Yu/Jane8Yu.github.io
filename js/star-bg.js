1f91a26daef5c3d1b70d18670963444061c44359
yuzhen <yzhen01.zy@gmail.com>
2017-06-12 08:05:12 +0800

    Site updated: 2017-06-12 08:05:12

--- js/star-bg.js
+++
@@ -1,6 +0,0 @@
-$(window).scroll(function(){
-  // $('.stars').height($('.stars').height()+$(window).scrollTop());
-  $('.stars').height($(document).height());
-  $('.twinkling').height($(document).height());
-  $('.clouds').height($(document).height());
-});
